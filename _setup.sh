6fefca7eefea870717ed174e7975ca434454b6f3
Thomas <thomas.0007@gmail.com>
2018-05-29 10:58:49 +0100

    Reuse a generic build_env_setup.bash Minor messages update

--- _setup.sh
+++ _setup.sh
@@ -4,17 +4,11 @@ if [[ -f ../_setup.sh  ]]; then
     source ../_setup.sh
 else
 
-    if [ -z "$build_path" ];
-        build_path="$HOME/tmp/buld"
-        mkdir -p $build_path
+    if [ ! -f build_env_setup.bash ]; then
+       wget -N -q https://raw.githubusercontent.com/thomaswhite/ubuntu-bash-files/master/bin/build_env_setup.bash
     fi
 
-    if  [  ! declare -F git_clone_or_checkout  ]; then
-      pushd $build_path
-      wget -nv -N https://raw.githubusercontent.com/thomaswhite/ubuntu-bash-files/master/.bash_functions_git
-      source .bash_functions_git
-      popd
-    fi
+    source build_env_setup.bash
 fi
 
 
--- build_1_exiftool.sh
+++ build_1_exiftool.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
-sudo apt-fast -qq  -y install lynx
+source _setup.sh "Install latest ExifTools"
+
+sudo apt-get -qq  -y install lynx
 
 link=`lynx -dump -listonly https://www.sno.phy.queensu.ca/~phil/exiftool/ | grep Image-ExifTool.*gz`
 dotPos=`expr index "$link" '.'`
 url=${link:$dotPos}
 
-pushd ${build_path:.}
-
 achive_name="$(basename $url)"
 tar="${achive_name%.*}"
 dir="${tar%.*}"
+ver=$(echo "$dir" | cut -d- -f3)
+
+pushd $build_path
 
 # remove build directory and the previously downloaded archive
 [[ -f $achive_name ]] && rm Image-ExifTool*.gz*
-[[ -d $dir      ]]    && find "$dir" -delete
+[[ -d $dir         ]] && find "$dir" -delete
 
 # download and unzip the
 wget -q  $url
@@ -25,11 +28,13 @@ pushd $dir
 perl Makefile.PL
 make
 #make test
-make install
+sudo make install
 popd
 
 # delete the archive and the installation directory
 [[ -f $achive_name ]] && rm Image-ExifTool*.gz*
 find "$dir" -delete
+echo ----------------------------------------------------------
+echo Currently installed ExifTool version is: `exiftool -ver`
 
 popd
